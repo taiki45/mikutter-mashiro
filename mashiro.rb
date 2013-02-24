bf84f460808df66d0288f95b45892684796275ce
Taiki ONO <taiks.4559@gmail.com>
2013-02-24 21:14:21 +0900

    init

---
+++ mashiro.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create :mashiro do
+  filter_update do |service, msgs|
+    [
+      service,
+      msgs.map do |msg|
+        msg[:message] = msg[:message].gsub /[ア-ヴ]/u, "ましろ"
+        msg
+      end
+    ]
+  end
+end
